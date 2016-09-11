80e289db516bcce4aba2dd2a83678a98475e7529
Hoang Le <lehuyhoang117@gmail.com>
2016-09-11 18:37:55 +0700

    tab bar

---
+++ Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  Connect
+//
+//  Created by Hoang Le on 6/12/16.
+//  Copyright © 2016 ping. All rights reserved.
+//
+
+#ifndef Connect_Bridging_Header_h
+#define Connect_Bridging_Header_h
+
+#import <ESTabBarController/ESTabBarController.h>
+//#import <UIColor-HexString/UIColor+HexString.h>
+
+#endif /* Connect_Bridging_Header_h */
