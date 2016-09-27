002e8e459f43891abd5263791db434a38af798f3
Hoang Le <lehuyhoang117@gmail.com>
2016-09-27 15:21:48 +0700

    load map

--- Group/MapController.swift
+++ Group/MapController.swift
@@ -9,40 +9,73 @@
 import UIKit
 import MapKit
 import AddressBook
+import RealmSwift
 
-class MapController: UIViewController, MKMapViewDelegate {
+class MapController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
-    var mapView: MKMapView!
+    var mapView = MKMapView()
     
     let regionRadius: CLLocationDistance = 1000
     
     var artworks = [Artwork]()
     
+    var points = [MKPointAnnotation]()
+    
+    let locationManager = CLLocationManager()
+    
+    var myLocation:CLLocationCoordinate2D?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-        let mapView = MKMapView()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
         
-        mapView.mapType = .Standard
         mapView.frame = view.bounds
+        mapView.mapType = .Standard
         mapView.height -= 50
         mapView.zoomEnabled = true
         mapView.scrollEnabled = true
         mapView.delegate = self
+        mapView.showsUserLocation = true
         view.addSubview(mapView)
         
-        // set initial location in Honolulu
-        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
-//        centerMapOnLocation(initialLocation)
+        if let coor = mapView.userLocation.location?.coordinate{
+            mapView.setCenterCoordinate(coor, animated: false)
+        }
         
-//        mapView.addAnnotations(artworks)
+        do {
+            let realm = try Realm()
+            let list = realm.objects(UserModel.self)
+            for data in list {
+                if data.clips.count > 0 {
+                    let clip = Clip(data: data.clips.last!)
+                    artworks.append(Artwork(clip: clip))
+                    
+//                    let point = MKPointAnnotation()
+//                    point.coordinate = CLLocationCoordinate2D(latitude: clip.lat, longitude: clip.long)
+//                    point.title = clip.lname
+//                    point.subtitle = clip.sublocal
+//                    points.append(point)
+                    
+                }
+            }
+            mapView.addAnnotations(artworks)
+        }
+        catch {
+            print(error)
+        }
         
+        
+
     }
-        
-    func centerMapOnLocation(location: CLLocation) {
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2.0, regionRadius * 2.0)
-        mapView.setRegion(coordinateRegion, animated: true)
+    
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: false)
+        locationManager.stopUpdatingLocation()
     }
     
     func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
@@ -59,6 +92,10 @@ class MapController: UIViewController, MKMapViewDelegate {
                 view.canShowCallout = true
                 view.calloutOffset = CGPoint(x: -5, y: 5)
                 view.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure) as UIView
+                
+                let url = NSURL(string: annotation.clip.thumb)
+                let data = NSData(contentsOfURL: url!) //make sure your image in this url does exist, otherwise unwrap in a if let check
+                view.image = UIImage(data: data!)
             }
             
             view.pinColor = annotation.pinColor()
@@ -78,28 +115,22 @@ class MapController: UIViewController, MKMapViewDelegate {
 
 class Artwork: NSObject, MKAnnotation {
     let title: String?
-    let locationName: String
-    let discipline: String
     let coordinate: CLLocationCoordinate2D
+    let clip: Clip
     
-    init(title: String, locationName: String, discipline: String, coordinate: CLLocationCoordinate2D) {
-        self.title = title
-        self.locationName = locationName
-        self.discipline = discipline
-        self.coordinate = coordinate
-        
+    init(clip: Clip) {
+        self.clip = clip
+        self.title = clip.lname
+        self.coordinate = CLLocationCoordinate2D(latitude: clip.lat, longitude: clip.long)
         super.init()
     }
 
     var subtitle: String? {
-        return locationName
+        return clip.sublocal
     }
     
-    // MARK: - MapKit related methods
-    
-    // pinColor for disciplines: Sculpture, Plaque, Mural, Monument, other
     func pinColor() -> MKPinAnnotationColor  {
-        switch discipline {
+        switch clip.city {
         case "Sculpture", "Plaque":
             return .Red
         case "Mural", "Monument":
